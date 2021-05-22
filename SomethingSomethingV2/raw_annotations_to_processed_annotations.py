25c94fd476edfd1e2566ee3a87fb78dd0c30bf97
RaivoKoot <raivokoot@gmail.com>
2021-05-22 13:03:28 +0100

    Add Something Something V2 helpers

---
+++ SomethingSomethingV2/raw_annotations_to_processed_annotations.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+"""
+This script takes in the raw json annotation files for 
+SomethingSomethingV2 and turns them into annotation.txt files
+that are compatible with this repository's dataloader VideoFrameDataset.
+
+Running this script requires that you already have the SomethingSomethingV2
+videos on disk as RGB frames, where each video has its own folder, containing
+the RGB frames of that video. For this, you can use 
+the script videos_to_frames.py.
+"""
+
+raw_annotations = 'something-something-v2-validation.json'
+out_file = 'something-something-v2-validation-processed.txt'
+labels_file = 'something-something-v2-labels.json'
+
+rgb_root = '../rgb/'
+
+annotations = None
+
+""" list containing the annotations for each sample """
+with open(raw_annotations) as file:
+    annotations = json.load(file)
+
+""" dictionary to go from [text label] -> [integer label] """
+with open(labels_file) as file:
+    labels_to_ids_dict = json.load(file)
+
+
+with open(out_file, 'w') as file:
+    for sample in annotations:
+        sample_id = sample['id']
+
+        """ find out the number of frames for this sample """
+        sample_rgb_directory = os.path.join(rgb_root,sample_id)
+        num_frames = len(os.listdir(sample_rgb_directory)) - 1
+
+        """ convert [text label] -> [integer label] """
+        text_label = sample['template'].replace('[', '').replace(']', '')
+        label_id = labels_to_ids_dict[text_label]
+
+        """ write to processed file """
+        annotation_string = "{} {} {} {}\n".format(sample_id, 0, num_frames, label_id)
+        file.write(annotation_string)
